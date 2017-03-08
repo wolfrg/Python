4bc458a2445a6224f1771f814982e1bfb190b01e
unknown <fengruigang2009@163.com>
2017-03-08 10:48:10 +0800

    update

--- test/test_paramiko.py
+++ test/test_paramiko.py
@@ -9,7 +9,7 @@ import paramiko
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='118.186.17.174',port=10001,username='root',password='frgadmin')
+ssh.connect(hostname='192.168.0.233',port=2222,username='root',password='123456')
 stdin,stdout,stderr=ssh.exec_command('ls  -l')
 
 for std in stdout.readlines():
